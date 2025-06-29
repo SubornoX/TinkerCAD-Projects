ad99eff47e141274b32a0b51e3d157a6f79b3d7b
Mahamudul Hasan Suborno <mahamudulhasanpersonal@gmail.com>
2025-06-29 14:46:09 +0600

    Add files via upload

---
+++ Traffic Light Demo Project using TinkerCAD and Arduino || Arduino Projects/traffic_light_demo_project_using_tinkercad_and_arduino_arduino_projects1.ino
@@ -0,0 +1,43 @@
+// C++ code
+
+int butt_state=0;
+const int butt_pin = 2;
+int Red=10;
+int Yellow=9;
+int Green=8;
+
+void setup()
+{
+  pinMode(Red, OUTPUT);
+  pinMode(Yellow, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(butt_pin, INPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  butt_state = digitalRead(butt_pin);
+  if(butt_state==1){
+    digitalWrite(Red,1);
+    Serial.print("Stop\n");
+    delay(2000);
+    digitalWrite(Red,0);
+    delay(500);
+    
+    digitalWrite(Yellow,1);
+    Serial.print("Get Ready\n");
+    delay(2000);
+    digitalWrite(Yellow,0);
+    delay(500);
+    
+    digitalWrite(Green,1);
+    Serial.print("Bye\n");
+    delay(2000);
+    digitalWrite(Green,0);
+    delay(500);
+    
+  }
+    
+  }
